ccf1b98f3410434b63e230a9848df7797bb028be
GarJor <45234206+GarJor@users.noreply.github.com>
2018-12-13 18:44:06 +0100

    Update and rename generado_partides.py to generador_partides.py

--- game/generador_partides.py
+++ game/generador_partides.py
@@ -28,14 +28,21 @@ myPlayer = input('\nNum del teu player: ')
 others = raw_input('Nums de la resta (sense espais, ex: 333): ')
 print("JUGADORS TRIATS: "+players[myPlayer]+players[int(others[0])]+players[int(others[1])]+players[int(others[2])])
 print('Inici de '+str(num_partides)+' partides...')
+system("nc -vz -w 1 localhost 9000 2> trash.txt")
+server = open("trash.txt","r")
+for line in server:
+	if "refused" in line:
+		server.close()
+		system("python -m SimpleHTTPServer 9000 > trash.txt 2> trash.txt &")
+		break
 time.sleep(2)
 for i in range(num_partides):
-	print("\n	Partida num: ",i)
 	rand_num = randint(1,1000)
+	print("\n	Partida num: "+str(i)+" [playing with seed "+str(rand_num)+"]")
 	game = fix+players[myPlayer]+players[int(others[0])]+players[int(others[1])]+players[int(others[2])]+"-s "
 	game += str(rand_num)
 	game += " -i MadMax/default.cnf -o default.res"
-	var = system(game)
+	var = system(game + ' 2> trash.txt')
 	res = open("default.res","r")
 	ultim = ""
 	for line in res:
@@ -49,10 +56,12 @@ for i in range(num_partides):
 			max = a
 	if max == 1:
 		guanyades += 1
+		print('\x1b[1;32m' +"	GAME WON" + '\x1b[0m')
 	else:
 		seeds.append(rand_num)
+		print('\x1b[1;31m' +"	GAME LOST" + '\x1b[0m')
+		print("	YOUR SCORE: "+str(list[1])+"\n	TOP SCORE: "+str(list[max]))
 
-system("clear")
 print("\n")
 for i in seeds:
 	print('\x1b[1;31m' +"[!]	perds amb seed = "+str(i) + '\x1b[0m')
@@ -61,8 +70,6 @@ print('\nJUGADORS TRIATS: '+players[myPlayer]+players[int(others[0])]+players[in
 print('\nRatio de partides guanyades = '+ str(ratio) +'% ('+str(guanyades)+'/'+str(num_partides)+')')
 repet = -2
 
-system("ps | grep python|grep python -n| grep 2: > trash.txt || python -m SimpleHTTPServer 9000 > trash.txt&")
-system("rm trash.txt")
 while(repet != -1):
 	if(repet != -2):
 		print('\n')
@@ -82,4 +89,5 @@ while(repet != -1):
 			veure = raw_input('\x1b[1;34m' +'\nVols visualitzar-la? (y/n) '+ '\x1b[0m' )
 			if(veure == 'y'):
 				system("/opt/google/chrome/google-chrome  http://localhost:9000/MadMax/Viewer/viewer.html?game=/default.res\&start=yes && clear");
-				#HAUREU DE CANVIAR AQUESTA ULTIMA FILA SI NO TENIU GOOGLE-CHROME
+
+system("rm -f trash.txt")
